2266d6bb370b62a4ee661cff8a004b91e59b0e40
Janko Krstic <keichiri@protonmail.com>
2018-08-05 23:25:25 +0200

    initial commit. piece storing and retrieving implemented

---
+++ pyrrent/storage.py
@@ -0,0 +1,113 @@
+import asyncio
+import logging
+import os
+import stat
+from concurrent.futures import ThreadPoolExecutor
+
+
+class StorageError(Exception):
+    pass
+
+
+class Storage:
+    @classmethod
+    def prepare(cls, base_path):
+        if not os.path.exists(base_path):
+            logging.info(f'Creating storage at path: {base_path}')
+
+            try:
+                os.makedirs(base_path)
+            except OSError as e:
+                raise StorageError(f'Failed to create storage at: {base_path}') from e
+        else:
+            logging.info(f'Checking permissions for path: {base_path}')
+            _check_ownership_and_permissions(base_path)
+
+        return cls(base_path)
+
+    def __init__(self, base_path):
+        self._base_path = base_path
+        self._handlers = {}
+
+    def create_handler_for_download(self, download_name, workers=3, loop=None):
+        logging.info(f'Creating handler for download {download_name}')
+        if download_name in self._handlers:
+            raise StorageError(f'Download {download_name} already active')
+
+        download_path = os.path.join(self._base_path, download_name)
+        handler = StorageHandler.create(download_path, workers, loop)
+        self._handlers[download_name] = handler
+
+        return handler
+
+    def remove_handler_for_download(self, download_name):
+        logging.info(f'Removing handler for download {download_name}')
+        self._handlers.pop(download_name, None)
+
+
+class StorageHandler:
+    @classmethod
+    def create(cls, path, workers, loop):
+        pieces_path = os.path.join(path, '.pieces')
+        if not os.path.exists(pieces_path):
+            try:
+                os.makedirs(pieces_path, 0o700)
+            except OSError as e:
+                raise StorageError(f'Failed to create pieces directory') from e
+
+        else:
+            _check_ownership_and_permissions(pieces_path)
+
+        return cls(path, pieces_path, workers, loop)
+
+
+    def __init__(self, path, pieces_path, workers, loop):
+        self._path = path
+        self._pieces_path = pieces_path
+        self._loop = loop or asyncio.get_event_loop()
+        self._pool = ThreadPoolExecutor(max_workers=workers)
+
+    async def store(self, piece_index, piece_data):
+        await self._loop.run_in_executor(self._pool, self._store, piece_index, piece_data)
+
+    async def retrieve(self, piece_index):
+        return await self._loop.run_in_executor(self._pool, self._retrieve, piece_index)
+
+    def _store(self, piece_index, piece_data):
+        piece_path = self._get_piece_path(piece_index)
+
+        try:
+            with open(piece_path, 'wb') as f:
+                f.write(piece_data)
+        except OSError as e:
+            raise StorageError(f'Failed to store piece at: {piece_path}') from e
+
+    def _retrieve(self, piece_index):
+        piece_path = self._get_piece_path(piece_index)
+
+        try:
+            with open(piece_path, 'rb') as f:
+                content = f.read()
+        except OSError as e:
+            raise StorageError(f'Failed to retrieve piece from: {piece_path}') from e
+
+        return content
+
+    def _get_piece_path(self, piece_index):
+        return os.path.join(self._pieces_path, f'{piece_index}.piece')
+
+
+def _check_ownership_and_permissions(path):
+    uid = os.getuid()
+
+    try:
+        file_stat = os.stat(path)
+    except OSError as e:
+        raise StorageError(f'Failed to check stat of path: {path}') from e
+
+    if file_stat.st_uid != uid:
+        raise StorageError(f'Current user not owner of path: {path}')
+
+    rw_flag = stat.S_IRUSR | stat.S_IWUSR
+    if file_stat.st_mode & rw_flag != rw_flag:
+        raise StorageError(f'Current user has no read-write permissions for path: {path}')
---
+++ tests/test_storage.py
@@ -0,0 +1,63 @@
+import asyncio
+import unittest
+import os
+import shutil
+
+from pyrrent.storage import Storage, StorageError
+
+
+class StorageTests(unittest.TestCase):
+    TEST_PATH = '/tmp/pyrrent/tests/storage'
+
+
+    def setUp(self):
+        if not os.path.exists(self.TEST_PATH):
+            os.makedirs(self.TEST_PATH)
+        else:
+            shutil.rmtree(self.TEST_PATH)
+        self.storage = Storage.prepare(self.TEST_PATH)
+        self.storage_handler = self.storage.create_handler_for_download('test_download')
+        self.loop = asyncio.get_event_loop()
+
+    def test_storage_raises_error_on_prepare_if_no_permissions(self):
+        path = '/bin/storage'
+
+        with self.assertRaises(StorageError):
+            Storage.prepare(path)
+
+    def test_create_handler_works_fine_if_pieces_already_exist(self):
+        path = '/tmp/pyrrent/tests/storage/test_download_2/.pieces'
+        os.makedirs(path)
+        piece_content = b'test_piece_data'
+        with open(os.path.join(path, '1.piece'), 'wb') as f:
+            f.write(piece_content)
+
+        self.storage.create_handler_for_download('test_download_2')
+
+        with open(os.path.join(path, '1.piece'), 'rb') as f:
+            current_piece_content = f.read()
+
+        self.assertEqual(piece_content, current_piece_content)
+
+    def test_store_piece(self):
+        piece_index = 1000
+        piece_data = b'test_piece_data'
+
+        self.loop.run_until_complete(self.storage_handler.store(piece_index, piece_data))
+
+        expected_piece_path = os.path.join(self.TEST_PATH, 'test_download/.pieces/1000.piece')
+        with open(expected_piece_path, 'rb') as f:
+            content = f.read()
+
+        self.assertEqual(content, piece_data)
+
+    def test_retrieve_piece(self):
+        piece_index = 1000
+        piece_content = b'test_piece_data'
+        piece_path = os.path.join(self.TEST_PATH, 'test_download/.pieces/1000.piece')
+        with open(piece_path, 'wb') as f:
+            f.write(piece_content)
+
+        retrieved_piece_content = self.loop.run_until_complete(self.storage_handler.retrieve(piece_index))
+
+        self.assertEqual(retrieved_piece_content, piece_content)
